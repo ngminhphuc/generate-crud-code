a7abf5abf6c903074e97cb1d9b35dd8b3cb1e4d8
guoguang0536@gmail.com <guoguang0536@gmail.com>
2021-03-08 21:18:59 +0800

    :bulb: 添加源码注释

--- src/com/madao/plugin/classes/ServiceImplClass.java
+++ src/com/madao/plugin/classes/ServiceImplClass.java
@@ -15,10 +15,30 @@ public class ServiceImplClass {
 //        String serviceName = entityClasses.getServiceClass().getName();
         String serviceName = entityClasses.getEntityName().concat("Service");
         StringBuilder content = new StringBuilder();
-        StringBuilder var10000 = content.append("private final ").append(entityClasses.getRepositoryClass().getName()).append(" repository; ").append("public " + serviceName + "(" + entityClasses.getRepositoryClass().getName() + " repository){ this.repository = repository;} ").append("public " + entityClasses.getEntityClass().getName() + " save(").append(entityClasses.getEntityClass().getName()).append(" entity)").append(" { return repository.save(entity);}").append("public List<" + entityClasses.getEntityClass().getName() + "> save(List<").append(entityClasses.getEntityClass().getName()).append("> entities) ")
-                .append("{ return (List<" + entityClasses.getEntityClass().getName() + ">) repository.saveAll(entities); }").append("public void deleteById(" + entityClasses.getIdType() + " id) { repository.deleteById(id); }").append("public Optional<").append(entityClasses.getEntityClass().getName()).append("> findById(" + entityClasses.getIdType() + " id) { ").append("return repository.findById(id);}").append("public List<").append(entityClasses.getEntityClass().getName()).append("> findAll() { return (List<" + entityClasses.getEntityClass().getName() + ">) repository.findAll(); }").append("public Page<").append(entityClasses.getEntityClass().getName()).append("> findAll(Pageable pageable) {").append("Page<" + entityClasses.getEntityClass().getName() + "> entityPage=repository.findAll(pageable);").append("List<" + entityClasses.getEntityClass().getName() + "> entities= entityPage.getContent();").append("return new PageImpl<>(entities, pageable, entityPage.getTotalElements());}").append("public " + entityClasses.getEntityClass().getName() + " update(");
+        StringBuilder var10000 = content.append("private final ")
+		        .append(entityClasses.getRepositoryClass().getName())
+		        .append(" repository; ")
+		        .append("public " + serviceName + "(" + entityClasses.getRepositoryClass().getName() + " repository){ this.repository = repository;} ").append("public " + entityClasses.getEntityClass().getName() + " save(").append(entityClasses.getEntityClass().getName()).append(" entity)").append(" { return repository.save(entity);}").append("public List<" + entityClasses.getEntityClass().getName() + "> save(List<").append(entityClasses.getEntityClass().getName()).append("> entities) ")
+                .append("{ return (List<" + entityClasses.getEntityClass().getName() + ">) repository.saveAll(entities); }")
+		        .append("public void deleteById(" + entityClasses.getIdType() + " id) { repository.deleteById(id); }")
+		        .append("public Optional<").append(entityClasses.getEntityClass().getName())
+		        .append("> findById(" + entityClasses.getIdType() + " id) { ")
+		        .append("return repository.findById(id);}")
+		        .append("public List<")
+		        .append(entityClasses.getEntityClass().getName())
+		        .append("> findAll() { return (List<" + entityClasses.getEntityClass().getName() + ">) repository.findAll(); }")
+		        .append("public Page<").append(entityClasses.getEntityClass().getName()).append("> findAll(Pageable pageable) {")
+		        .append("Page<" + entityClasses.getEntityClass().getName() + "> entityPage=repository.findAll(pageable);")
+		        .append("List<" + entityClasses.getEntityClass().getName() + "> entities= entityPage.getContent();")
+		        .append("return new PageImpl<>(entities, pageable, entityPage.getTotalElements());}")
+		        .append("public " + entityClasses.getEntityClass().getName() + " update(");
+
         String var10001 = entityClasses.getEntityClass().getName();
-        var10000.append(var10001 + " entity," + entityClasses.getIdField().getType().getPresentableText() + " id){  Optional<" + entityClasses.getEntityClass().getName() + "> optional=findById(id);").append("if (optional.isPresent()){").append("return save(entity); }  return null;}");
+
+        var10000.append(var10001 + " entity," + entityClasses.getIdField().getType().getPresentableText()
+		        + " id){  " + entityClasses.getEntityClass().getName() + " data=findById(id).orElseThrow(ResourceNotFoundException::new);")
+		        .append("BeanUtil.copyProperties(data, entity);")
+		        .append("return save(entity); }");
         return content.toString();
     }
 }
